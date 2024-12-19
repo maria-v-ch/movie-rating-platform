12f286ceb85c1e1e70a3bf2492462592200b147e
maria-v-ch <m.chuvtaeva@gmail.com>
2024-12-19 20:13:18 +0000

    fix: properly configure templates in test settings

--- config/test_settings.py
+++ config/test_settings.py
@@ -51,12 +51,26 @@ LOGGING = {
 # Disable debug mode
 DEBUG = False
 
-# Use fast template loader
-TEMPLATES[0]['OPTIONS']['loaders'] = [
-    ('django.template.loaders.cached.Loader', [
-        'django.template.loaders.filesystem.Loader',
-        'django.template.loaders.app_directories.Loader',
-    ]),
+# Configure templates for testing
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+            'loaders': [
+                ('django.template.loaders.cached.Loader', [
+                    'django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader',
+                ]),
+            ],
+        },
+    },
 ]
 
 # Disable CSRF for tests
